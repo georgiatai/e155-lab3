349e4ce321bf852828f3cf8575d4d6743639022b
georgiatai <161803507+georgiatai@users.noreply.github.com>
2025-09-24 03:22:26 -0700

    move src to the right folder

---
+++ fpga/src/testbench/testbench_keypad_decoder.sv
@@ -0,0 +1,10 @@
+/////////////////////////
+// lab3_gt.sv
+// Author: Georgia Tai, ytai@g.hmc.edu
+// Date: Sep 14, 2025
+// 
+// Top level module for Lab 3 of E155 @HMC. 
+// Takes in a 4-by-4 keypad and uses dual seven segment display to 
+// show the most recent two inputs.
+/////////////////////////
+
